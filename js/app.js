a0140eb0d3a388410a2d62ee00b6e20d848113f6
sara shahraki <sarashahraki.79@gmail.com>
2024-07-22 12:54:09 +0330

    added project tabagh16

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let nav = document.getElementById("header_nav");
+let burger = document.getElementById("hamberger_icon");
+
+burger.addEventListener("click", function (){
+    burger.style.display = "none"
+    nav.style.display = "block"
+});
